59d76456c5e13e5a1b158e6e313fe9f2d4dcb8e6
Борковский Владимир Владимирович <gr692_bvv@ttit.local>
2023-02-20 16:37:19 +0700

    ability electro

--- ability/electrolyte_bullet/electrolyte_bullet.go
+++ ability/electrolyte_bullet/electrolyte_bullet.go
@@ -1,23 +1,8 @@
-components {
-  id: "enemy1_script"
-  component: "/enemy/enemy1/enemy1.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/enemy/enemy1/enemy1.tilesource\"\n"
-  "default_animation: \"enemy1\"\n"
+  data: "tile_set: \"/resources/images.atlas\"\n"
+  "default_animation: \"electrolet\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -37,35 +22,30 @@ embedded_components {
   id: "collisionobject"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
-  "mass: 0.0\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"enemy\"\n"
-  "mask: \"bullet\"\n"
-  "mask: \"hero\"\n"
+  "group: \"bullet\"\n"
   "mask: \"enemy\"\n"
-  "mask: \"electrolyte\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
+  "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
   "      x: 1.0\n"
-  "      y: -2.0\n"
+  "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
+  "      x: 0.101577334\n"
+  "      y: 0.08320967\n"
+  "      z: -0.008526353\n"
+  "      w: 0.99130493\n"
   "    }\n"
   "    index: 0\n"
-  "    count: 3\n"
+  "    count: 1\n"
   "  }\n"
-  "  data: 5.5\n"
-  "  data: 5.5\n"
-  "  data: 10.0\n"
+  "  data: 21.915\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
